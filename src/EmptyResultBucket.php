3e25f9ca7a85c7c05a72c004407f13da501248c0
Grégory Planchat <gregory@kiboko.fr>
2023-11-14 12:38:59 +0100

    Updated the contracts

--- src/EmptyResultBucket.php
+++ src/EmptyResultBucket.php
@@ -9,28 +9,8 @@ use Kiboko\Contract\Bucket as Contract;
 /**
  * @template Type of non-empty-array<array-key, mixed>|object
  *
- * @implements Contract\AcceptanceResultBucketInterface<Type>
- * @implements Contract\RejectionResultBucketInterface<Type>
+ * @implements Contract\ResultBucketInterface<Type>
  */
-final class EmptyResultBucket implements Contract\AcceptanceResultBucketInterface, Contract\RejectionResultBucketInterface
+final class EmptyResultBucket implements Contract\ResultBucketInterface
 {
-    public function reasons(): ?array
-    {
-        return null;
-    }
-
-    public function exceptions(): ?array
-    {
-        return null;
-    }
-
-    public function walkAcceptance(): iterable
-    {
-        return new \EmptyIterator();
-    }
-
-    public function walkRejection(): iterable
-    {
-        return new \EmptyIterator();
-    }
 }
